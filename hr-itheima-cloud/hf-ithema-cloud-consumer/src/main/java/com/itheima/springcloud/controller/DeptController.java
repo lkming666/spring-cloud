8e847ed790bd1c428220d57920ba2b9de6e5e123
lkm <1352346506@qq.com>
2019-08-07 19:45:08 +0800

    8获取注册中心信息

--- hr-itheima-cloud/hf-ithema-cloud-consumer/src/main/java/com/itheima/springcloud/controller/DeptController.java
+++ hr-itheima-cloud/hf-ithema-cloud-consumer/src/main/java/com/itheima/springcloud/controller/DeptController.java
@@ -8,25 +8,30 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/consumer")
+@RequestMapping("/dept")
 public class DeptController {
     @Autowired
     private RestTemplate restTemplate;
 
     private static final String REST_URL_PREFIX = "http://localhost:8001";
 
-    @PostMapping(value = "/dept/add")
+    @PostMapping(value = "/add")
     public boolean add(@RequestBody Dept dept) {
         return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
     }
 
-    @GetMapping(value = "/dept/get/{id}")
+    @GetMapping(value = "/get/{id}")
     public Dept get(@PathVariable("id") Long id) {
         return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id,Dept.class);
     }
 
-    @GetMapping(value = "/dept/list")
+    @GetMapping(value = "/list")
     public List<Dept> list() {
         return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list",List.class);
+    }
+
+    @GetMapping("/provider")
+    public Object discovery(){
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/provider", Object.class);
     }
 }
--- hr-itheima-cloud/hf-ithema-cloud-provider/src/main/java/com/itheima/hf/controller/DeptController.java
+++ hr-itheima-cloud/hf-ithema-cloud-provider/src/main/java/com/itheima/hf/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.itheima.hf.controller;
 import com.itheima.hf.model.Dept;
 import com.itheima.hf.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,5 +28,20 @@ public class DeptController {
     @GetMapping(value = "/list")
     public List<Dept> list() {
         return service.findAll();
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/provider")
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();//获得服务
+        System.out.println(list);
+        List<ServiceInstance> insList = discoveryClient.getInstances("HF-ITHEIMA-CLOUD-PROVIDER");//服务名称
+        for (ServiceInstance si:insList) {
+            System.out.println(si.getHost() +"," + si.getServiceId() +"," +si.getPort()
+                    +"," +si.getUri() +"," +si.getMetadata());
+        }
+        return this.discoveryClient;
     }
 }
