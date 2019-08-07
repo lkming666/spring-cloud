8f103cef0660789056e57394da151902e61e9556
lkm <1352346506@qq.com>
2019-08-07 18:45:01 +0800

    在注册中心查询服务信息

--- hr-itheima-cloud/hf-ithema-cloud-provider/src/main/java/com/itheima/hf/ProviderApplication.java
+++ hr-itheima-cloud/hf-ithema-cloud-provider/src/main/java/com/itheima/hf/ProviderApplication.java
@@ -3,9 +3,11 @@ package com.itheima.hf;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.itheima.hf.mapper")
+@EnableEurekaClient
 public class ProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class,args);
